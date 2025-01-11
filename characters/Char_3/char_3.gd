ecafecaa8ff164186fb05808454562e598577377
Layton <laytonb@hotmail.co.uk>
2025-01-11 17:24:49 +0000

    example with two characters controlled separately

--- characters/Char_3/char_3.gd
+++ characters/Char_3/char_3.gd
@@ -5,6 +5,11 @@ extends CharacterBody2D
 @export var JUMP_VELOCITY: float
 @export var AIR_MOVEMENT_FRACTION: float
 
+@export var PLAYER_NUMBER: int
+
+
+@onready var ACTION: Dictionary = $/root/Utils.get_keyboard_actions(PLAYER_NUMBER)
+
 
 func _ready() -> void:
     %AnimationPlayer.play("RESET")
@@ -16,12 +21,12 @@ func _physics_process(delta: float) -> void:
         velocity += get_gravity() * delta
 
     # Handle jump.
-    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+    if Input.is_action_just_pressed(ACTION["jump"]) and is_on_floor():
         velocity.y = JUMP_VELOCITY
 
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
-    var direction := Input.get_axis("ui_left", "ui_right")
+    var direction := Input.get_axis(ACTION["left"], ACTION["right"])
     if direction:
         if is_on_floor():
             velocity.x = direction * SPEED
--- global_scripts/utils.gd
+++ global_scripts/utils.gd
@@ -5,3 +5,15 @@ func is_facing_left(node: Node2D) -> bool:
     assert(node)
 
     return node.scale.y == -1 and abs(node.rotation_degrees) == 180
+
+
+func get_keyboard_actions(player_number: int) -> Dictionary:
+    assert(player_number >= 1 and player_number <= 2)
+
+    return {
+        "left": "p1_move_left" if player_number == 1 else "p2_move_left",
+        "right": "p1_move_right" if player_number == 1 else "p2_move_right",
+        "jump": "p1_jump" if player_number == 1 else "p2_jump",
+        "punch": "p1_punch" if player_number == 1 else "p2_punch",
+        "parry": "p1_parry" if player_number == 1 else "p2_parry",
+    }
